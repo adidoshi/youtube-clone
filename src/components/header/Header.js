f97f2e9e12168304162e8ae1a1dd751f71a201cf
Aditya Doshi <80141865+adidoshi@users.noreply.github.com>
2021-12-16 20:50:06 +0530

    refactor: img src

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -24,7 +24,7 @@ const Header = ({ handleToggleSidebar }) => {
         onClick={() => handleToggleSidebar()}
       />
       <img
-        src="http://pngimg.com/uploads/youtube/youtube_PNG2.png"
+        src="https://pngimg.com/uploads/youtube/youtube_PNG2.png"
         alt="..."
         className="header_logo"
       />
